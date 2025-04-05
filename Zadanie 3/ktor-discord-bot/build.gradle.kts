6fda92238b86ab51f116653a4bc0b7fbb6d6eeb4
Stanisław Mnich <stanislaw.mnich02@gmail.com>
2025-04-05 15:16:29 +0200

    Commit na 3.0

--- Zadanie 3/ktor-discord-bot/build.gradle.kts
+++ Zadanie 3/ktor-discord-bot/build.gradle.kts
@@ -8,14 +8,10 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-server-core:2.3.6")
-    implementation("io.ktor:ktor-server-netty:2.3.6")
-    implementation("io.ktor:ktor-server-call-logging:2.3.6")
-    implementation("io.ktor:ktor-client-core:2.3.6")
-    implementation("io.ktor:ktor-client-cio:2.3.6")
-    implementation("io.ktor:ktor-client-content-negotiation:2.3.6")
-    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.6")
-    implementation("ch.qos.logback:logback-classic:1.4.11")
+    implementation("io.ktor:ktor-client-core:2.3.4")
+    implementation("io.ktor:ktor-client-cio:2.3.4")
+    implementation("io.ktor:ktor-client-content-negotiation:2.3.4")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.4")
 }
 
 application {
--- Zadanie 3/ktor-discord-bot/src/main/kotlin/Main.kt
+++ Zadanie 3/ktor-discord-bot/src/main/kotlin/Main.kt
@@ -1,3 +1,24 @@
-fun main() {
-    println("====== Bot odpalony! ======")
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.coroutines.*
+
+fun main() = runBlocking {
+    val botToken = "Bot ***"
+    val channelId = "***"
+
+    val client = HttpClient(CIO)
+
+    val response: HttpResponse = client.post("https://discord.com/api/v10/channels/$channelId/messages") {
+        headers {
+            append(HttpHeaders.Authorization, botToken)
+            append(HttpHeaders.ContentType, "application/json")
+        }
+        setBody("""{"content": "Cześć, jestem botem z Kotlina!"}""")
+    }
+
+    println("Response status: ${response.status}")
+    client.close()
 }
