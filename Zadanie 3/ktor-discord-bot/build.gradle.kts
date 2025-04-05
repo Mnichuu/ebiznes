818ebcc219fb8a83f3bce9a65726d8f773a7e8cf
Stanisław Mnich <stanislaw.mnich02@gmail.com>
2025-04-05 15:33:50 +0200

    security update

--- Zadanie 3/ktor-discord-bot/build.gradle.kts
+++ Zadanie 3/ktor-discord-bot/build.gradle.kts
@@ -8,7 +8,6 @@ repositories {
 }
 
 dependencies {
-    implementation("io.ktor:ktor-client-core:2.3.4")
     implementation("io.ktor:ktor-client-cio:2.3.4")
     implementation("io.ktor:ktor-client-content-negotiation:2.3.4")
     implementation("io.ktor:ktor-serialization-kotlinx-json:2.3.4")
--- Zadanie 3/ktor-discord-bot/src/main/kotlin/Main.kt
+++ Zadanie 3/ktor-discord-bot/src/main/kotlin/Main.kt
@@ -4,10 +4,21 @@ import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import kotlinx.coroutines.*
+import java.io.FileInputStream
+import java.util.Properties
+
+
+fun loadProperties(): Properties {
+    val properties = Properties().apply {
+        load(FileInputStream("src/main/resources/config.properties"))
+    }
+    return properties
+}
 
 fun main() = runBlocking {
-    val botToken = "Bot ***"
-    val channelId = "***"
+    val properties = loadProperties()
+    val botToken = properties.getProperty("BOT_TOKEN") ?: throw Exception("BOT_TOKEN not found in config.properties")
+    val channelId = properties.getProperty("CHANNEL_ID") ?: throw Exception("CHANNEL_ID not found in config.properties")
 
     val client = HttpClient(CIO)
 
