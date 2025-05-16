a38b5299516157aef87824d8a9da8ed5723de3ed
stanislaw <stanislaw.mnich@student.uj.edu.pl>
2025-05-16 18:32:18 +0200

    endpoint obsługujący logowanie bez Oauth2

--- Zadanie 4/app/main.go
+++ Zadanie 4/app/main.go
@@ -5,14 +5,39 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"net/http"
 )
 
 var db *gorm.DB
 var err error
 
+type User struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+func login(c echo.Context) error {
+	u := new(User)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+
+	if u.Email == "admin@admin.pl" || u.Password == "admin" {
+		return c.JSON(http.StatusOK, map[string]string{
+			"token": "example-jwt-token",
+		})
+	}
+
+	return c.JSON(http.StatusUnauthorized, map[string]string{
+		"message": "Invalid credentials",
+	})
+}
+
 func main() {
 	// Inicjalizacja Echo
 	e := echo.New()
+
+	e.POST("/login", login)
 
 	// Inicjalizacja gorm i SQLite
 	db, err = gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
